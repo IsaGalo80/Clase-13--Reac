9461204f9d18dbfcc4d2e1d4772cc4e85ec4136b
IsaGalo80 <isagalo1980@outlook.com>
2022-05-19 17:13:25 +0000

    Que lindo es el reac

---
+++ src/js/component/card.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import img from "src/img/descarga (1).jpg"
+
+const Card = () => {
+	return (
+		<div class="col-3 ">
+			<div class="card">
+				<img
+					src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fkena.com%2Fdescarga-tu-mandala-y-empieza-a-liberar-estres%2F&psig=AOvVaw0WDI34XVcbWd7lHa714W21&ust=1653064887482000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCICXvuaA7PcCFQAAAAAdAAAAABAK"
+					class="card-img-top"
+					alt=""
+				/>
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div class="card">
+				<img
+					src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mandalaweb.com%2Fmandalas%2Fcoloreados%2Fde-animales%2F&psig=AOvVaw1o0xjBDQUTgVh4QiIrTqlB&ust=1653064861927000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNiwu_2D7PcCFQAAAAAdAAAAABAN/640/360"
+					class="card-img-top"
+					alt=""
+				/>
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div><div class="card">
+				<img
+					src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.es%2FDecorativo-Elefante-120x120cm-Pegatina-Decorativa%2Fdp%2FB07JLMJM7P&psig=AOvVaw24oJT_FOuWy9HW8qBICZff&ust=1653065804448000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCcr5KE7PcCFQAAAAAdAAAAABAF/640/360"
+					class="card-img-top"
+					alt=""
+				/>
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div><div class="card">
+				<img
+					src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com.mx%2Fpin%2F319051954859047987%2F&psig=AOvVaw0CgIL3cXehHgSeATECvqGL&ust=1653065902985000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNC0rsKE7PcCFQAAAAAdAAAAABAD/640/360"
+					class="card-img-top"
+					alt=""
+				/>
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#" class="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
